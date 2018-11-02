58a1ce471f523172828971f547664515510c1f0b
Carlos <carlosperezurena@gmail.com>
2018-11-02 18:50:28 -0400

    CRUD created

--- grails-app/init/quiz2_grails/BootStrap.groovy
+++ grails-app/init/quiz2_grails/BootStrap.groovy
@@ -3,6 +3,7 @@ package quiz2_grails
 class BootStrap {
 
     def init = { servletContext ->
+        new User(cdeula: '40223847837', name: 'Juanito', lastName: 'Rodríguez', email: 'juanito1@gmail.com', bornDate: new Date(1990,11,02))
     }
     def destroy = {
     }
