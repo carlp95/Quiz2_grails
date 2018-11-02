7245bb1daf4a634366439408f547ac7450e18598
Carlos <carlosperezurena@gmail.com>
2018-11-02 19:24:51 -0400

    Implementing spring security

--- grails-app/domain/quiz2_grails/User.groovy
+++ grails-app/domain/quiz2_grails/User.groovy
@@ -1,17 +1,50 @@
 package quiz2_grails
 
-class User {
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+
+@EqualsAndHashCode(includes = 'cedula')
+@ToString(includes = 'cedula', includeNames = true, includePackage = false)
+class User implements Serializable{
+
+    private static final long serialVersionUID = 1
+
+    transient springSecurityService
 
     String cedula
+    String password
     String name
     String lastName
     String email
     Date bornDate
 
+    Set<Roles> getAuthorities() {
+        UserRoles.findAllByUsuario(this)*.roles
+    }
+
+    def beforeInsert() {
+        encodePassword()
+    }
+
+    def beforeUpdate() {
+        if (this.isDirty('password')) { //TODO: verificar en en foro.
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+    }
+
+    static transients = ['springSecurityService']
+
+
     static constraints = {
         cedula maxSize: 13
         name maxSize: 255
-        cedula(cedula: true)
+        password blank: false, password: true
+        cedula blank: false, unique: true
         name(name: true)
         lastName(lastName: true)
         email(email: true)
--- grails-app/init/quiz2_grails/BootStrap.groovy
+++ grails-app/init/quiz2_grails/BootStrap.groovy
@@ -3,7 +3,12 @@ package quiz2_grails
 class BootStrap {
 
     def init = { servletContext ->
-        new User(cdeula: '40223847837', name: 'Juanito', lastName: 'Rodríguez', email: 'juanito1@gmail.com', bornDate: new Date(1990,11,02))
+        User user = new User(cedula: '40223847837', name: 'Juanito', lastName: 'Rodríguez', email: 'juanito1@gmail.com', bornDate: new Date(1990,11,02)).save(failOnError: true)
+
+        def adminRole = new Roles(authority: 'ROLE_ADMIN').save( failOnError: true )
+        def userRole = new Roles(authority: 'ROLE_USER').save( failOnError: true )
+
+        UserRoles.create( user,adminRole )
     }
     def destroy = {
     }
